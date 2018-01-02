8ee9ec6f0a30c2c0a4882436a4205ec812207756
karthik0311 <karthikeee0311@yahoo.in>
2018-01-02 21:06:02 +0530

    changes in app module

--- src/app/app.module.ts
+++ src/app/app.module.ts
@@ -4,11 +4,12 @@ import { ErrorHandler, NgModule } from '@angular/core';
 import { IonicApp, IonicErrorHandler, IonicModule } from 'ionic-angular';
 import { SplashScreen } from '@ionic-native/splash-screen';
 import { StatusBar } from '@ionic-native/status-bar';
-import { AngularFireAuth } from 'angularfire2/auth';
+
 import { AngularFireModule } from 'angularfire2';
 import { MyApp } from './app.component';
 import { config } from './app.firebaseconfig';
 import { AuthProvider } from '../providers/auth/auth';
+import { AngularFireAuthModule } from 'angularfire2/auth';
 
 @NgModule({
   declarations: [
@@ -19,7 +20,8 @@ import { AuthProvider } from '../providers/auth/auth';
     BrowserModule,
     IonicModule.forRoot(MyApp),
     AngularFireModule.initializeApp(config),
-    AngularFireAuth,
+    AngularFireAuthModule
+    
     
   ],
   bootstrap: [IonicApp],
